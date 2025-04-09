122aa71bad463feb672f4d23ab53dd8d9fc1054e
SHUBHAM ADLINGE <124245410+shubhamadlinge@users.noreply.github.com>
2025-04-09 19:17:53 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "chatbot20.com" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-northeast-2"
   }
 }
